5a09040f5321b4a75b9aa57b94b65df1c22bc890
PavelErsh <pavel204812@gmail.com>
2021-11-11 11:19:00 +0200

    add player

--- game.py
+++ game.py
@@ -5,28 +5,35 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Star Wars"
 
+class Player(arcade.Sprite):
+    def update(self):
+        if self.right > SCREEN_WIDTH:
+            self.right = SCREEN_WIDTH
+        if self.left < 0:
+            self.left = 0
 
 # класс с игрой
 class MyGame(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         self.background = arcade.load_texture("img//bacground.png")
+        self.player = Player("img//player.png", 0.5)
 
 
     # начальные значения
     def setup(self):
-        pass
+        self.player.center_x = SCREEN_WIDTH / 2
+        self.player.center_y = 70
 
     # отрисовка
     def on_draw(self):
         arcade.start_render()
-        arcade.set_background_color(arcade.color.AMAZON)
 
         arcade.draw_texture_rectangle(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, SCREEN_WIDTH, SCREEN_HEIGHT, self.background )
-
+        self.player.draw()
     # игровая логика
     def update(self, delta_time):
-        pass
+        self.player.update()
 
 
 window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
